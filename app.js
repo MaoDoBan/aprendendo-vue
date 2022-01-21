0af9fbee14da52d064886888d5b5ce0d4739906b
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-01-21 11:42:07 -0300

    inserindo no elemento html dados dinamicamente

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("BAH");
+const app = Vue.createApp({
+  //template: "<h2>Eu sou a template!</h2>"
+  data(){
+    return {
+      titulo: "A volta dos que não foram",
+      autor: "Aquele Cara",
+      idade: 44
+    };
+  }
+});
+
+app.mount("#div-bah");
